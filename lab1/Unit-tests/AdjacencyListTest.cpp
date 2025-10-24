8b4b02a36bb471c8f4591ea8b5bd0f39bcd70d54
anzhela <angela.tes3@gmail.com>
2025-10-24 22:31:54 +0300

    Deleted edge->markActive() from makeEdge. Ids are now received only from addVertex() method. When initialising graph, directness is mentioned. Used doctest::Approx instead of '=='. Added new test_case for boundary and error conditions check

--- lab1/Unit-tests/AdjacencyListTest.cpp
+++ lab1/Unit-tests/AdjacencyListTest.cpp
@@ -7,7 +7,6 @@
 
 static Edge* makeEdge(Vertex* from, Vertex* to, double dist = 10.0, RoadType type = RoadType::ROAD, RoadCharacteristic rc = RoadCharacteristic::STANDARD) {
     Edge* edge = new Edge(from, to, dist, type, rc);
-    edge->markActive();
     return edge;
 }
 
@@ -42,9 +41,9 @@ TEST_CASE("addEdge, getNeighbors, getEdge and getEdgesFrom undirected") {
     auto* c2 = makeCity("B", 2000);
     auto* c3 = makeCity("C", 1500);
 
-    g.addVertex(c1);
-    g.addVertex(c2);
-    g.addVertex(c3);
+    int id1 = g.addVertex(c1);
+    int id2 = g.addVertex(c2);
+    int id3 = g.addVertex(c3);
 
     auto* e1 = makeEdge(c1, c2, 5.0, RoadType::ROAD);
     auto* e2 = makeEdge(c2, c3, 7.0, RoadType::RAIL);
@@ -53,29 +52,25 @@ TEST_CASE("addEdge, getNeighbors, getEdge and getEdgesFrom undirected") {
     g.addEdge(e2);
 
     SUBCASE("getNeighbors") {
-        auto n1 = g.getNeighbors(c1->getId());
-        auto n2 = g.getNeighbors(c2->getId());
-        auto n3 = g.getNeighbors(c3->getId());
+        auto n1 = g.getNeighbors(id1);
+        auto n2 = g.getNeighbors(id2);
+        auto n3 = g.getNeighbors(id3);
 
         CHECK(n1.size() == 1);
-        CHECK(n1[0] == c2->getId());
-
+        CHECK(n1[0] == id2);
         CHECK(n2.size() == 2);
-        CHECK(std::find(n2.begin(), n2.end(), c1->getId()) != n2.end());
-        CHECK(std::find(n2.begin(), n2.end(), c3->getId()) != n2.end());
-
+        CHECK(std::find(n2.begin(), n2.end(), id1) != n2.end());
+        CHECK(std::find(n2.begin(), n2.end(), id3) != n2.end());
         CHECK(n3.size() == 1);
-        CHECK(n3[0] == c2->getId());
+        CHECK(n3[0] == id2);
     }
-
     SUBCASE("getEdge") {
-        CHECK(g.getEdge(c1->getId(), c2->getId()) == e1);
-        CHECK(g.getEdge(c2->getId(), c1->getId()) == e1);
-        CHECK(g.getEdge(c1->getId(), c3->getId()) == nullptr);
+        CHECK(g.getEdge(id1, id2) == e1);
+        CHECK(g.getEdge(id2, id1) == e1);
+        CHECK(g.getEdge(id1, id3) == nullptr);
     }
-
     SUBCASE("getEdgesFrom") {
-        auto edgesFrom2 = g.getEdgesFrom(c2->getId());
+        auto edgesFrom2 = g.getEdgesFrom(id2);
         CHECK(edgesFrom2.size() == 2);
         CHECK((edgesFrom2[0] == e1 || edgesFrom2[1] == e1));
         CHECK((edgesFrom2[0] == e2 || edgesFrom2[1] == e2));
@@ -87,21 +82,21 @@ TEST_CASE("directed edge") {
 
     auto* a = makeCity("A", 1000);
     auto* b = makeCity("B", 2000);
-    g.addVertex(a);
-    g.addVertex(b);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
 
     auto* e = makeEdge(a, b, 12.0, RoadType::ROAD);
     g.addEdge(e);
 
-    auto nA = g.getNeighbors(a->getId());
-    auto nB = g.getNeighbors(b->getId());
+    auto nA = g.getNeighbors(id_a);
+    auto nB = g.getNeighbors(id_b);
 
     CHECK(nA.size() == 1);
-    CHECK(nA[0] == b->getId());
+    CHECK(nA[0] == id_b);
     CHECK(nB.empty());
 
-    CHECK(g.getEdge(a->getId(), b->getId())->getDistance() == 12.0);
-    CHECK(g.getEdge(b->getId(), a->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b)->getDistance() == doctest::Approx(12.0));
+    CHECK(g.getEdge(id_b, id_a) == nullptr);
 }
 
 TEST_CASE("removeEdge and removeVertex") {
@@ -110,40 +105,40 @@ TEST_CASE("removeEdge and removeVertex") {
     auto* b = makeCity("B", 2000);
     auto* c = makeCity("C", 1500);
 
-    g.addVertex(a);
-    g.addVertex(b);
-    g.addVertex(c);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
+    int id_c = g.addVertex(c);
 
     auto* e1 = makeEdge(a, b, 5.0);
     auto* e2 = makeEdge(b, c, 7.0);
     auto* e3 = makeEdge(a, c, 15.0);
-
     g.addEdge(e1);
     g.addEdge(e2);
     g.addEdge(e3);
 
     g.removeEdge(e1);
     CHECK_FALSE(e1->isActive());
-    CHECK(g.getEdge(a->getId(), b->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b) == nullptr);
 
-    g.removeVertex(b->getId());
+    g.removeVertex(id_b);
     CHECK_FALSE(b->isActive());
     CHECK_FALSE(e2->isActive());
-    CHECK(g.getEdge(a->getId(), c->getId()) == e3);
+    CHECK(g.getEdge(id_a, id_c) == e3);
 }
 
 TEST_CASE("getEdge correctness and invalid IDs") {
     AdjacencyList<City, Edge> g(true);
     auto* a = makeCity("A", 1000);
     auto* b = makeCity("B", 2000);
-    g.addVertex(a);
-    g.addVertex(b);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
 
     auto* e = makeEdge(a, b);
     g.addEdge(e);
 
-    CHECK(g.getEdge(a->getId(), b->getId()) == e);
-    CHECK(g.getEdge(b->getId(), a->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b) == e);
+    CHECK(g.getEdge(id_b, id_a) == nullptr);
+
     CHECK(g.getEdge(5, 1) == nullptr);
     CHECK(g.getEdge(-1, 0) == nullptr);
 }
@@ -170,5 +165,30 @@ TEST_CASE("edge distances sum") {
     for (auto* edge : g.getEdges()) {
         if (edge->isActive()) total += edge->getDistance();
     }
-    CHECK(total == 55.0);
+    CHECK(total == doctest::Approx(55.0));
+}
+
+TEST_CASE("boundary and error conditions") {
+    AdjacencyList<City, Edge> g(false);
+    auto* a = makeCity("A");
+    int id_a = g.addVertex(a);
+
+    CHECK(g.getNeighbors(-1).empty());
+    CHECK(g.getNeighbors(100).empty());
+    CHECK(g.getEdge(-1, id_a) == nullptr);
+    CHECK(g.getEdge(id_a, 100) == nullptr);
+    CHECK(g.getEdgesFrom(-1).empty());
+    CHECK(g.getEdgesFrom(100).empty());
+    CHECK(g.getVertexById(-1) == nullptr);
+    CHECK(g.getVertexById(100) == nullptr);
+
+    CHECK(g.addVertex(nullptr) == -1);
+    g.addEdge(nullptr);
+    g.removeEdge(nullptr);
+
+    g.removeVertex(-1);
+    g.removeVertex(100);
+
+    CHECK(g.getNumberOfVertices() == 1);
+    CHECK(g.getVertexById(id_a) == a);
 }
--- lab1/Unit-tests/AdjacencyMatrixTest.cpp
+++ lab1/Unit-tests/AdjacencyMatrixTest.cpp
@@ -1,10 +1,8 @@
 #include "doctest.h"
 
-#include "AdjacencyList.h"
 #include "AdjacencyMatrix.h"
 #include "City.h"
 #include "Edge.h"
-#include "GraphAlgorithm.h"
 #include <algorithm>
 
 static Edge* makeEdge(Vertex* from, Vertex* to, double dist = 10.0, RoadType type = RoadType::ROAD, RoadCharacteristic rc = RoadCharacteristic::STANDARD) {
@@ -12,7 +10,6 @@ static Edge* makeEdge(Vertex* from, Vertex* to, double dist = 10.0, RoadType typ
     edge->markActive();
     return edge;
 }
-
 static City* makeCity(const std::string& name, long population = 1000) {
     return new City(name, population);
 }
@@ -22,14 +19,14 @@ TEST_CASE("addVertex, addEdge, directed") {
     auto* a = makeCity("A");
     auto* b = makeCity("B");
 
-    g.addVertex(a);
-    g.addVertex(b);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
 
     auto* e = makeEdge(a, b, 8.0);
     g.addEdge(e);
 
-    CHECK(g.getEdge(a->getId(), b->getId()) == e);
-    CHECK(g.getEdge(b->getId(), a->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b) == e);
+    CHECK(g.getEdge(id_b, id_a) == nullptr);
 
     CHECK(a->isActive());
     CHECK(b->isActive());
@@ -41,21 +38,21 @@ TEST_CASE("undirected") {
     auto* a = makeCity("A");
     auto* b = makeCity("B");
 
-    g.addVertex(a);
-    g.addVertex(b);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
 
     auto* e = makeEdge(a, b, 12.0);
     g.addEdge(e);
 
-    CHECK(g.getEdge(a->getId(), b->getId()) == e);
-    CHECK(g.getEdge(b->getId(), a->getId()) == e);
+    CHECK(g.getEdge(id_a, id_b) == e);
+    CHECK(g.getEdge(id_b, id_a) == e);
 
-    auto nA = g.getNeighbors(a->getId());
-    auto nB = g.getNeighbors(b->getId());
+    auto nA = g.getNeighbors(id_a);
+    auto nB = g.getNeighbors(id_b);
     CHECK(nA.size() == 1);
     CHECK(nB.size() == 1);
-    CHECK(nA[0] == b->getId());
-    CHECK(nB[0] == a->getId());
+    CHECK(nA[0] == id_b);
+    CHECK(nB[0] == id_a);
 }
 
 TEST_CASE("removeEdge and removeVertex") {
@@ -64,9 +61,9 @@ TEST_CASE("removeEdge and removeVertex") {
     auto* b = makeCity("B");
     auto* c = makeCity("C");
 
-    g.addVertex(a);
-    g.addVertex(b);
-    g.addVertex(c);
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
+    int id_c = g.addVertex(c);
 
     auto* e1 = makeEdge(a, b);
     auto* e2 = makeEdge(b, c);
@@ -75,40 +72,63 @@ TEST_CASE("removeEdge and removeVertex") {
 
     g.removeEdge(e1);
     CHECK_FALSE(e1->isActive());
-    CHECK(g.getEdge(a->getId(), b->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b) == nullptr);
 
-    g.removeVertex(b->getId());
+    g.removeVertex(id_b);
     CHECK_FALSE(b->isActive());
     CHECK_FALSE(e2->isActive());
-    CHECK(g.getEdge(a->getId(), b->getId()) == nullptr);
-    CHECK(g.getEdge(b->getId(), c->getId()) == nullptr);
+    CHECK(g.getEdge(id_a, id_b) == nullptr);
+    CHECK(g.getEdge(id_b, id_c) == nullptr);
 }
 
-TEST_CASE("getNeighbors, edges sum and invalid ID") {
+TEST_CASE("edges sum and invalid ID") {
     AdjacencyMatrix<City, Edge> g(true);
     auto* a = makeCity("A");
     auto* b = makeCity("B");
     auto* c = makeCity("C");
-    g.addVertex(a);
-    g.addVertex(b);
-    g.addVertex(c);
+
+    int id_a = g.addVertex(a);
+    int id_b = g.addVertex(b);
+    int id_c = g.addVertex(c);
 
     auto* e1 = makeEdge(a, b, 10.0);
     auto* e2 = makeEdge(a, c, 20.0);
     g.addEdge(e1);
     g.addEdge(e2);
 
-    auto nA = g.getNeighbors(a->getId());
+    auto nA = g.getNeighbors(id_a);
     CHECK(nA.size() == 2);
-    CHECK(std::find(nA.begin(), nA.end(), b->getId()) != nA.end());
-    CHECK(std::find(nA.begin(), nA.end(), c->getId()) != nA.end());
+    CHECK(std::find(nA.begin(), nA.end(), id_b) != nA.end());
+    CHECK(std::find(nA.begin(), nA.end(), id_c) != nA.end());
 
     double sum = 0;
     for (auto* edge : g.getEdges()) {
         if (edge->isActive()) sum += edge->getDistance();
     }
-    CHECK(sum == 30.0);
+    CHECK(sum == doctest::Approx(30.0));
+}
 
+TEST_CASE("boundary and error conditions") {
+    AdjacencyMatrix<City, Edge> g(true);
+    auto* a = makeCity("A");
+    int id_a = g.addVertex(a);
+
+    CHECK(g.getEdge(-1, id_a) == nullptr);
+    CHECK(g.getEdge(id_a, 100) == nullptr);
+    CHECK(g.getEdgesFrom(-1).empty());
+    CHECK(g.getEdgesFrom(100).empty());
     CHECK(g.getNeighbors(-1).empty());
     CHECK(g.getNeighbors(100).empty());
+    CHECK(g.getVertexById(-1) == nullptr);
+    CHECK(g.getVertexById(100) == nullptr);
+
+    CHECK(g.addVertex(nullptr) == -1);
+    g.addEdge(nullptr);
+    g.removeEdge(nullptr);
+
+    g.removeVertex(-1);
+    g.removeVertex(100);
+
+    CHECK(g.getNumberOfVertices() == 1);
+    CHECK(g.getVertexById(id_a) == a);
 }
